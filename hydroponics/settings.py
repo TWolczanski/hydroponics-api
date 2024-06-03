6a92141794951a07869d8617e3ea7bb73661fd3b
Tomek <tomasz.wolczanski@proton.me>
2024-06-04 01:27:41 +0200

    specified `DEFAULT_AUTHENTICATION_CLASSES` in settings.py

--- hydroponics/settings.py
+++ hydroponics/settings.py
@@ -119,6 +119,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     "TEST_REQUEST_DEFAULT_FORMAT": "json",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
 }
 
 
