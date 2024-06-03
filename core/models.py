b91545c71e2f561072474460aafde7fc7badab34
Tomek <tomasz.wolczanski@proton.me>
2024-06-03 17:50:34 +0200

    added models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class HydroponicSystem(models.Model):
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="hydroponic_systems"
+    )
+    created_at = models.DateTimeField(default=timezone.now)
+
+
+class SensorReading(models.Model):
+    ph = models.DecimalField(max_digits=4, decimal_places=2)
+    water_temp = models.DecimalField(max_digits=5, decimal_places=2)
+    tds = models.DecimalField(max_digits=7, decimal_places=2)
+    hydroponic_system = models.ForeignKey(
+        HydroponicSystem, on_delete=models.CASCADE, related_name="sensor_readings"
+    )
+    created_at = models.DateTimeField(default=timezone.now)
