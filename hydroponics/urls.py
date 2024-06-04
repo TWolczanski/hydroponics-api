a6e9350db69e5ccc019fdcf5a8d93c29eb00b38e
Tomek <tomasz.wolczanski@proton.me>
2024-06-04 13:58:14 +0200

    updated urls

--- hydroponics/urls.py
+++ hydroponics/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from core.views import HydroponicSystemViewSet, SensorReadingViewSet
+
+router = routers.DefaultRouter()
+router.register(
+    r"hydroponic_systems", HydroponicSystemViewSet, basename="hydroponic-system"
+)
+router.register(r"sensor_readings", SensorReadingViewSet, basename="sensor-reading")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("dj_rest_auth.urls")),
     path("auth/registration/", include("dj_rest_auth.registration.urls")),
 ]
+
+urlpatterns += router.urls
