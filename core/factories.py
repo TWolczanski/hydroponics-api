5390df869283f63622f58677b887dd05d13b17e8
Tomek <tomasz.wolczanski@proton.me>
2024-06-04 14:17:48 +0200

    added factories

---
+++ core/factories.py
@@ -0,0 +1,25 @@
+import factory
+from core.models import HydroponicSystem, SensorReading, User
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+
+class HydroponicSystemFactory(factory.django.DjangoModelFactory):
+    plant_count = factory.Faker("pyint", min_value=1, max_value=20)
+    owner = factory.SubFactory(UserFactory)
+
+    class Meta:
+        model = HydroponicSystem
+
+
+class SensorReadingFactory(factory.django.DjangoModelFactory):
+    ph = factory.Faker("pydecimal", right_digits=2, min_value=-5, max_value=15)
+    water_temp = factory.Faker("pydecimal", right_digits=2, min_value=0, max_value=100)
+    tds = factory.Faker("pydecimal", right_digits=2, min_value=0, max_value=40000)
+    hydroponic_system = factory.SubFactory(HydroponicSystemFactory)
+
+    class Meta:
+        model = SensorReading
